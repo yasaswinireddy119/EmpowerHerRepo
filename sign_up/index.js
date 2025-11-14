dc13946e22ecd68bbe837b1d83b63ff1f0158b45
yasaswinireddy119 <yasaswinireddykotha@gmail.com>
2025-11-14 21:50:23 +0530

    anotherbranch

--- sign_up/index.js
+++ sign_up/index.js
@@ -8,4 +8,17 @@ function signup(userName){
         return "Signup Sucessfull, Please Login"
     }
 }
-    
+
+function login(userName, password){
+    arr=["seeta","hanuma","rama"]
+    if(!arr.includes(userName)){
+        return "User Not Found, Please Signup"
+        
+    }
+     if (password === "Emp@123") {
+        return "Login Sucessfull..."
+    } else {
+        return "Wrong Password...."
+    }
+}
+}
